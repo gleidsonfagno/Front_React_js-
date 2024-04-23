3ff59be7e4470e4da605b9c6f77a89479ab4f6d7
gledsonfagno <gleidsonfagner838@gmail.com>
2024-04-23 20:26:47 -0300

    :lipstick: feat: Estilização CSS do Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,41 @@
+import Logosvg from "../../svg/logo.svg";
+
 function Header() {
-  return <h1>Header</h1>;
+  const descLogo = "Essa é a logo do blog";
+  return (
+    <>
+      <header className="container flex-space-between">
+        <div className="logo ">
+          <img src={Logosvg} alt={descLogo} />
+        </div>
+
+        <div className="search">
+          <input type="text" className="input-search" name="search" />
+        </div>
+
+        <div className="menu">
+          <li>
+            {" "}
+            <a href="/" className="nav-link">
+              Categories
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="/" className="nav-link">
+              About
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="/" className="nav-link">
+              Contact
+            </a>{" "}
+          </li>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
