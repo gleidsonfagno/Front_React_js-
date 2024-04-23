701e3674833b0b60387351d6c3ea23d66e0d70e7
gledsonfagno <gleidsonfagner838@gmail.com>
2024-04-23 20:53:40 -0300

    :lipstick: feat: Estilização CSS do Article e Posts

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,10 +2,14 @@ import Post from "../Post";
 
 function Article() {
   return (
-    <>
+    <section className="container">
       <h1>Article</h1>
+      <div className="mt-3 container-posts flex">
       <Post />
-    </>
+      <Post />
+      <Post />
+      </div>
+    </section>
   );
 }
 
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,26 @@
 function Post() {
-  return <h1>Post</h1>;
+  return (
+    <div className="post mx-2">
+      <div className="img-post"></div>
+      <h5>Tecnologia</h5>
+      <h3>O guia definitivo do blog</h3>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        ullamcorper, lectus in ornare facilisis, justo nunc elementum nisl, eget
+        congue libero ligula eu leo. In dapibus hendrerit nisi, nec consectetur
+        leo convallis ut.
+      </p>
+      <div className="flex pt-2">
+        <div className="img-profile">
+          <img src="https://github.com/gleidsonfagno.png" alt="img" />
+        </div>
+        <div className="description-profile ml-3">
+          <h6 className="color-blue">Gleidson fagno</h6>
+          <p>Aug 2, 2020 - 8 min read</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
